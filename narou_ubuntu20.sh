0176884d78a0d8532ac4b444adb2b24029f3b8de
Kota Ito <kotai@mafut.com>
2023-04-02 06:36:10 +0000

    update narou.rb

--- narou_ubuntu20.sh
+++ narou_ubuntu20.sh
@@ -19,16 +19,29 @@ function init(){
     
     # https://github.com/whiteleaf7/narou
     # https://github.com/kyukyunyorituryo/AozoraEpub3
+    # https://qiita.com/hirohiro77/items/13ef7354042967e352c4
 
     # Install Ruby Java
     mkdir narou
     cd narou
-    apt-get -y --force-yes install ruby-full default-jdk
+    apt-get -y --allow install ruby-full default-jdk epub-utils calibre
     gem install narou
     wget https://github.com/kyukyunyorituryo/AozoraEpub3/releases/download/v1.1.1b14Q/AozoraEpub3-1.1.1b14Q.zip
     unzip AozoraEpub3-1.1.1b14Q.zip
+    cd AozoraEpub3-1.1.1b14Q
+    cat << EOF > kindlegen
+#!/bin/sh
+INPUTFILE="$3"
+OUTPUTFILE=`echo "$3"|sed 's/\.epub/\.mobi/g'`
+/usr/bin/ebook-convert "${INPUTFILE}" "${OUTPUTFILE}"
+EOF
+    chmod 777 kindlegen
 
     narou init
+    narou s convert.no-strip=true
+    narou s convert.no-open=true
+    narou s convert.no-mobi=true
+    narou s convert.copy_to="/home/kotai/epub"
 }
 
 if [ $# = 0 ];

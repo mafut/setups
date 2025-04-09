1a77f44d50e327e6c394d03d3a6f7a76de4d22f2
Kota Ito <kotai@mafut.com>
2025-04-08 23:17:37 -0700

    add display driver from https://github.com/hyphenlee/jdi-drm-rpi

--- rpi-colorberry/setup.sh
+++ rpi-colorberry/setup.sh
@@ -5,11 +5,16 @@ DIR_COMMON=$(
     pwd
 )
 source ${DIR_COMMON}/setup64-debian12.sh
+apt-get -y install raspberrypi-kernel raspberrypi-kernel-headers
 
 # Display
 # https://github.com/hyphenlee/jdi-drm-rpi
 # https://github.com/hyphenlee/jdi-drm-rpi/blob/main/jdi-drm-rpi-debian11-32.zip
 # https://github.com/hyphenlee/jdi-drm-rpi/blob/main/jdi-drm-rpi-debian12-64-6.6.62%2Brpt-rpi-v8.zip
+
+unzip jdi-drm-rpi.zip -d /var/tmp/
+cd /var/tmp/jdi-drm-rpi
+make install
 
 # Keyboard
 # https://github.com/ardangelo/beepberry-keyboard-driver

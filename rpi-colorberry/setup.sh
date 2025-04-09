06c50c42976694110d876c51e47db717897e3b6c
Kota Ito <kotai@mafut.com>
2025-04-08 23:23:49 -0700

    fix path

--- rpi-colorberry/setup.sh
+++ rpi-colorberry/setup.sh
@@ -7,12 +7,16 @@ DIR_COMMON=$(
 source ${DIR_COMMON}/setup64-debian12.sh
 apt-get -y install raspberrypi-kernel raspberrypi-kernel-headers
 
+DIR_SELF=$(
+    cd $(dirname $0)
+    pwd
+)
 # Display
 # https://github.com/hyphenlee/jdi-drm-rpi
 # https://github.com/hyphenlee/jdi-drm-rpi/blob/main/jdi-drm-rpi-debian11-32.zip
 # https://github.com/hyphenlee/jdi-drm-rpi/blob/main/jdi-drm-rpi-debian12-64-6.6.62%2Brpt-rpi-v8.zip
 
-unzip jdi-drm-rpi.zip -d /var/tmp/
+unzip ${DIR_SELF}/jdi-drm-rpi.zip -d /var/tmp/
 cd /var/tmp/jdi-drm-rpi
 make install
 

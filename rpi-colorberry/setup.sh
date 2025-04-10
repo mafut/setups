924a2e03bef9274bcec9479f10b4b75c6f5b5984
Kota Ito <kotai@mafut.com>
2025-04-09 18:58:31 -0700

    merge 32 and 64 setup

--- rpi-colorberry/setup.sh
+++ rpi-colorberry/setup.sh
@@ -4,15 +4,18 @@ DIR_COMMON=$(
     cd ../rpi-common/
     pwd
 )
-source ${DIR_COMMON}/setup64-debian12.sh
+source ${DIR_COMMON}/setup-debian11.sh
 apt-get -y install raspberrypi-kernel raspberrypi-kernel-headers
+
+# OS
+# https://github.com/ardangelo/beepberry-buildroot -> https://github.com/michaelstepner/beepy-buildroot/actions -> no apt
 
 # Display
 # https://github.com/hyphenlee/jdi-drm-rpi
 # https://github.com/hyphenlee/jdi-drm-rpi/blob/main/jdi-drm-rpi-debian11-32.zip
 # https://github.com/hyphenlee/jdi-drm-rpi/blob/main/jdi-drm-rpi-debian12-64-6.6.62%2Brpt-rpi-v8.zip
 
-unzip ${DIR_SELF}/jdi-drm-rpi.zip -d /var/tmp/
+unzip ${DIR_SELF}/jdi-drm-rpi-debian11-32.zip -d /var/tmp/
 cd /var/tmp/jdi-drm-rpi
 make install
 

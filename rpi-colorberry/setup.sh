2dfa351aad08e9fae240f857716d92b346801eb0
Kota Ito <kotai@mafut.com>
2025-04-10 11:52:13 -0700

    add crontab setup

--- rpi-colorberry/setup.sh
+++ rpi-colorberry/setup.sh
@@ -11,13 +11,26 @@ source ${DIR_COMMON}/setup.sh
 
 # Display
 # https://github.com/hyphenlee/jdi-drm-rpi
-unzip ${DIR_SELF}/jdi-drm-rpi-debian11-32.zip -d /var/tmp/ -o
-cd /var/tmp/jdi-drm-rpi
-make install
+if [ ! -e /boot/overlays/sharp-drm.dtbo ]; then
+    unzip ${DIR_SELF}/jdi-drm-rpi-debian11-32.zip -d /var/tmp/ -o
+    cd /var/tmp/jdi-drm-rpi
+    make install
+fi
+
+# Keyboard
+# https://github.com/ardangelo/beepberry-keyboard-driver -> didn't work
+# https://github.com/sqfmi/bbqX0kbd_driver -> worked
+if [ ! -e /boot/overlays/beepy-kbd.dtbo ]; then
+    unzip ${DIR_SELF}/beepy-kbd.zip -d /var/tmp/ -o
+    cd /var/tmp/bbqX0kbd-driver-main
+    make install
+fi
 
 # Backlight
 cp -f ${DIR_SELF}/backlit.py /usr/local/sbin/backlit.py
 chmod +x /usr/local/sbin/backlit.py
+echo "@reboot   sleep 5;/path/to/back.py" >/var/tmp/crontab.txt
+crontab /var/tmp/crontab.txt
 
 cat <<EOF >>${FILE_BASHALIASES}
 alias d0="echo 0 | sudo tee /sys/module/sharp_drm/parameters/dither"
@@ -28,8 +41,3 @@ alias d4="echo 4 | sudo tee /sys/module/sharp_drm/parameters/dither"
 alias by="echo 1 | sudo tee /sys/module/sharp_drm/parameters/backlit"
 alias bn="echo 0 | sudo tee /sys/module/sharp_drm/parameters/backlit"
 EOF
-
-# Keyboard
-# https://github.com/ardangelo/beepberry-keyboard-driver -> didn't work
-# https://github.com/sqfmi/bbqX0kbd_driver -> worked
-unzip ${DIR_SELF}/beepy-kbd.zip -d /var/tmp/ -o

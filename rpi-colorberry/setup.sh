63b374a673bf3d21af109740072a88387dcec061
Kota Ito <kotai@mafut.com>
2025-04-11 10:56:34 -0700

    remove unnecessary commands

--- rpi-colorberry/setup.sh
+++ rpi-colorberry/setup.sh
@@ -30,8 +30,6 @@ if [ ! -e /boot/overlays/beepy-kbd.dtbo ]; then
     cd /var/tmp/bbqX0kbd_driver-main
     make install
 fi
-rm -f /etc/console-setup/cached_setup_keyboard.sh
-dpkg-reconfigure keyboard-configuration
 
 # Backlight
 cp -f ${DIR_SELF}/side-button.py /usr/local/sbin/side-button.py
